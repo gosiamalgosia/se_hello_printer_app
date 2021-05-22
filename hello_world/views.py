698b29c015be6c768b2917d53bf0efa7c4c013ab
gosiamalgosia <m.wysokinska.m@gmail.com>
2021-05-22 13:59:15 +0200

    travis-er

--- hello_world/views.py
+++ hello_world/views.py
@@ -3,7 +3,7 @@ from hello_world.formater import get_formatted
 from hello_world.formater import SUPPORTED, PLAIN
 from flask import request
 
-moje_imie = "Gosia"
+moje_imie = "Malgosia"
 msg = "Hello World!"
 
 
